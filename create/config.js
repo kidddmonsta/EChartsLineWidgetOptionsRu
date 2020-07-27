01a060d93b77eaaef4ffe9e5e260b9386c29151d
kidddMonsta <nikolas.yeap@gmail.com>
2020-07-27 13:59:19 +0300

    ECharts Line Widget Options Ru

---
+++ create/config.js
@@ -0,0 +1,15 @@
+const CODE_LANGUAGES = ["RU", "EN"];  //коды переключателя языков
+
+const IP_TO_INDICATOR_CORE = "http://10.0.3.209:7002/api/v2/indicator-core/indicator/";
+
+const IP_TO_ANALYT_QUERY_CORE = "http://10.0.3.209:7003/api/v2/analyt-query-core/registry/analyt-query";
+
+const IP_GENERATE_SQL = "http://10.0.3.209:7003/api/v2/analyt-query-core/";
+
+const IP_EXECUTE_SQL = "http://10.0.3.209:7003/api/v2/sql-query-core/";
+
+const IP_ANALIT_QUERY_LIST = "http://10.0.3.209:7003/api/v2/analyt-query-core/analyt-query/list";
+
+const IP_WIDGET_CRUD = "http://10.0.3.209:7005/api/v2/widget-core/registry/widget";
+
+const IP_TO_ANALYT_QUERY_RESULTS = "http://10.0.3.209:7003/api/v2/analyt-query-core/analyt-query/";
